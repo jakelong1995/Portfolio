fcd7bbcbd964fb91185d7bfb3f2442ca9ada18eb
jakelong1995 <hoanglong2579@gmail.com>
2023-09-16 18:02:58 +0700

    sat 169

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './portfolio/src/**/*.{html,js}',
-    './portfolio/index.html'
+    './src/**/*.{html,js}',
+    './index.html'
   ],
 
   theme: {
